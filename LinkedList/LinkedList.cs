eee447e70c46d7f109749f5dced3a3fca3495704
Petr Barabáš <petr.barabas@astratex.com>
2024-09-13 17:13:26 +0200

    Add project files.

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList<T>
+    {
+        private Node<T>? node = null;
+        //private List<Node<T>> nodes = [];
+
+        public Node<T> First { get; set; } = null!;
+        public Node<T>? Last { get; set; }
+
+        public Node<T> AddLast(Node<T> nodeToAddLast) 
+        {
+            if (nodeToAddLast.next is null && nodeToAddLast.data is not null)
+            {
+                Last = nodeToAddLast;
+            }
+
+            return Last;
+        }
+
+        public Node<T> AddFirst(Node<T> nodeToAddFirst) 
+        {
+            node = nodeToAddFirst;
+
+            if(node.next is not null)
+            {
+
+                First = node;
+            }
+
+            return First;
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<char> linkedList = new();
+            Node<char> node3 = new() 
+            { 
+                data = 'C',
+            };
+            
+            Node<char> node2 = new() 
+            { 
+                data = 'B',
+                next = node3
+            };
+            
+            Node<char> node1 = new() 
+            { 
+                data = 'A',
+                next = node2
+            };
+
+            linkedList.AddFirst(node1);
+            linkedList.AddFirst(node2);
+            //linkedList.AddFirst(node1);
+            Console.WriteLine($"The first node is: {linkedList.First.data}");
+        }
+    }
+}
